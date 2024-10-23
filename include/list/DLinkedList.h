c450aca3c38f800c6f433c127d4f9c24fbf65117
lamphnam <nampham2131@gmail.com>
2024-10-23 17:50:49 +0700

    upload all initial

---
+++ include/list/DLinkedList.h
@@ -0,0 +1,582 @@
+/*
+ * File:   DLinkedList.h
+ */
+
+#ifndef DLINKEDLIST_H
+#define DLINKEDLIST_H
+
+#include <iostream>
+#include <sstream>
+#include <type_traits>
+
+#include "IList.h"
+
+using namespace std;
+
+template <class T>
+class DLinkedList : public IList<T> {
+ public:
+  class Node;         // Forward declaration
+  class Iterator;     // Forward declaration
+  class BWDIterator;  // Forward declaration
+
+ protected:
+  Node* head;  // this node does not contain user's data
+  Node* tail;  // this node does not contain user's data
+  int count;
+
+  bool (*itemEqual)(T& lhs, T& rhs);  // function pointer: test if two items
+                                      // (type: T&) are equal or not
+  void (*deleteUserData)(DLinkedList<T>*);  // function pointer: be called to remove items (if they
+                         // are pointer type)
+
+ public:
+  DLinkedList(void (*deleteUserData)(DLinkedList<T>*) = 0,
+              bool (*itemEqual)(T&, T&) = 0);
+
+  DLinkedList(const DLinkedList<T>& list);
+
+  DLinkedList<T>& operator=(const DLinkedList<T>& list);
+
+  ~DLinkedList();
+
+  // Inherit from IList: BEGIN
+  void add(T e);
+
+  void add(int index, T e);
+
+  T removeAt(int index);
+
+  bool removeItem(T item, void (*removeItemData)(T) = 0);
+
+  bool empty();
+
+  int size();
+
+  void clear();
+
+  T& get(int index);
+
+  int indexOf(T item);
+
+  bool contains(T item);
+
+  string toString(string (*item2str)(T&) = 0);
+
+  // Inherit from IList: END
+
+  void println(string (*item2str)(T&) = 0) {
+    cout << toString(item2str) << endl;
+  }
+
+  void setDeleteUserDataPtr(void (*deleteUserData)(DLinkedList<T>*) = 0) {
+    this->deleteUserData = deleteUserData;
+  }
+
+  bool contains(T array[], int size) {
+    int idx = 0;
+    for (DLinkedList<T>::Iterator it = begin(); it != end(); it++) {
+      if (!equals(*it, array[idx++], this->itemEqual)) return false;
+    }
+    return true;
+  }
+
+  /*
+   * free(DLinkedList<T> *list):
+   *  + to remove user's data (type T, must be a pointer type, e.g.: int*,
+   * Point*)
+   *  + if users want a DLinkedList removing their data,
+   *      he/she must pass "free" to constructor of DLinkedList
+   *      Example:
+   *      DLinkedList<T> list(&DLinkedList<T>::free);
+   */
+  static void free(DLinkedList<T>* list) {
+    typename DLinkedList<T>::Iterator it = list->begin();
+    while (it != list->end()) {
+      delete *it;
+      it++;
+    }
+  }
+
+  /* begin, end and Iterator helps user to traverse a list forwardly
+   * Example: assume "list" is object of DLinkedList
+
+   DLinkedList<char>::Iterator it;
+   for(it = list.begin(); it != list.end(); it++){
+          char item = *it;
+          std::cout << item; //print the item
+   }
+   */
+  Iterator begin() { return Iterator(this, true); }
+
+  Iterator end() { return Iterator(this, false); }
+
+  /* last, beforeFirst and BWDIterator helps user to traverse a list backwardly
+   * Example: assume "list" is object of DLinkedList
+
+   DLinkedList<char>::BWDIterator it;
+   for(it = list.last(); it != list.beforeFirst(); it--){
+          char item = *it;
+          std::cout << item; //print the item
+   }
+   */
+  BWDIterator bbegin() { return BWDIterator(this, true); }
+
+  BWDIterator bend() { return BWDIterator(this, false); }
+
+ protected:
+  static bool equals(T& lhs, T& rhs, bool (*itemEqual)(T&, T&)) {
+    if (&lhs == nullptr || &rhs == nullptr) return false;
+    if (itemEqual == 0)
+      return lhs == rhs;
+    else
+      return itemEqual(lhs, rhs);
+  }
+
+  void copyFrom(const DLinkedList<T>& list);
+
+  void removeInternalData();
+
+  Node* getPreviousNodeOf(int index);
+
+  //////////////////////////////////////////////////////////////////////
+  ////////////////////////  INNER CLASSES DEFNITION ////////////////////
+  //////////////////////////////////////////////////////////////////////
+ public:
+  class Node {
+   public:
+    T data;
+    Node* next;
+    Node* prev;
+    friend class DLinkedList<T>;
+
+   public:
+    Node(Node* next = 0, Node* prev = 0) {
+      this->next = next;
+      this->prev = prev;
+    }
+
+    Node(T data, Node* next = 0, Node* prev = 0) {
+      this->data = data;
+      this->next = next;
+      this->prev = prev;
+    }
+  };
+
+  //////////////////////////////////////////////////////////////////////
+  class Iterator {
+   private:
+    DLinkedList<T>* pList;
+    Node* pNode;
+
+   public:
+    Iterator(DLinkedList<T>* pList = 0, bool begin = true) {
+      if (begin) {
+        if (pList != 0)
+          this->pNode = pList->head->next;
+        else
+          pNode = 0;
+      } else {
+        if (pList != 0)
+          this->pNode = pList->tail;
+        else
+          pNode = 0;
+      }
+      this->pList = pList;
+    }
+
+    Iterator& operator=(const Iterator& iterator) {
+      this->pNode = iterator.pNode;
+      this->pList = iterator.pList;
+      return *this;
+    }
+
+    void remove(void (*removeItemData)(T) = 0) {
+      pNode->prev->next = pNode->next;
+      pNode->next->prev = pNode->prev;
+      Node* pNext = pNode->prev;  // MUST prev, so iterator++ will go to end
+      if (removeItemData != 0) removeItemData(pNode->data);
+      delete pNode;
+      pNode = pNext;
+      pList->count -= 1;
+    }
+
+    T& operator*() { return pNode->data; }
+
+    bool operator!=(const Iterator& iterator) {
+      return pNode != iterator.pNode;
+    }
+
+    // Prefix ++ overload
+    Iterator& operator++() {
+      pNode = pNode->next;
+      return *this;
+    }
+
+    // Postfix ++ overload
+    Iterator operator++(int) {
+      Iterator iterator = *this;
+      ++*this;
+      return iterator;
+    }
+  };
+  class BWDIterator {
+   private:
+    DLinkedList<T>* pList;
+    Node* pNode;
+
+   public:
+    BWDIterator(DLinkedList<T>* pList = 0, bool begin = true) {
+      if (begin) {
+        if (pList != 0)
+          this->pNode = pList->tail->prev;  
+        else
+          pNode = 0;
+      } else {
+        if (pList != 0)
+          this->pNode =
+              pList->head;  
+        else
+          pNode = 0;
+      }
+      this->pList = pList;
+    }
+
+    BWDIterator& operator=(const BWDIterator& iterator) {
+      this->pNode = iterator.pNode;
+      this->pList = iterator.pList;
+      return *this;
+    }
+    
+    void remove(void (*removeItemData)(T) = 0) {
+      pNode->prev->next = pNode->next;
+      pNode->next->prev = pNode->prev;
+      Node* pPrev = pNode->next; 
+      if (removeItemData != 0) removeItemData(pNode->data);
+      delete pNode;
+      pNode = pPrev;
+      pList->count -= 1;
+    }
+    
+    T& operator*() { return pNode->data; }
+
+    bool operator!=(const BWDIterator& iterator) {
+      return pNode != iterator.pNode;
+    }
+
+    BWDIterator& operator--() {
+      pNode = pNode->prev;
+      return *this;
+    }
+
+    BWDIterator operator--(int) {
+      BWDIterator iterator = *this;
+      --*this;
+      return iterator;
+    }
+  };
+};
+
+//////////////////////////////////////////////////////////////////////
+// Define a shorter name for DLinkedList:
+
+template <class T>
+using List = DLinkedList<T>;
+
+//////////////////////////////////////////////////////////////////////
+////////////////////////     METHOD DEFNITION      ///////////////////
+//////////////////////////////////////////////////////////////////////
+
+template <class T>
+DLinkedList<T>::DLinkedList(void (*deleteUserData)(DLinkedList<T>*),
+                            bool (*itemEqual)(T&, T&)) {
+  this->deleteUserData = deleteUserData;
+  this->itemEqual = itemEqual;
+
+  head = new Node();
+  tail = new Node();
+
+  head->next = tail;
+  tail->prev = head;
+
+  count = 0;
+}
+
+template <class T>
+DLinkedList<T>::DLinkedList(const DLinkedList<T>& list) {
+  // TODO
+
+  this->deleteUserData = list.deleteUserData;
+  this->itemEqual = list.itemEqual;
+
+  head = new Node();
+  tail = new Node();
+
+  head->next = tail;
+  tail->prev = head;
+
+  count = 0;
+
+  Node* current = list.head->next;
+  while (current != list.tail) {
+    add(current->data);
+    current = current->next;
+  }
+}
+
+template <class T>
+DLinkedList<T>& DLinkedList<T>::operator=(const DLinkedList<T>& list) {
+  if (this != &list) {
+    
+    clear();  
+
+    this->deleteUserData = list.deleteUserData;
+    this->itemEqual = list.itemEqual;
+
+    head = new Node();
+    tail = new Node();
+    head->next = tail;
+    tail->prev = head;
+    count = 0;
+
+    Node* current = list.head->next;
+    while (current != list.tail) {
+      add(current->data);
+      current = current->next;
+    }
+  }
+  return *this;
+}
+
+template <class T>
+DLinkedList<T>::~DLinkedList() {
+  clear();
+  delete head;
+  delete tail;
+}
+
+template <class T>
+void DLinkedList<T>::add(T e) {
+  // TODO
+  Node* newNode = new Node(e);
+
+  newNode->prev = tail->prev;
+  newNode->next = tail;
+  tail->prev->next = newNode;
+  tail->prev = newNode;
+  count++;
+}
+
+template <class T>
+void DLinkedList<T>::add(int index, T e) {
+  if (index < 0 || index > count) {
+    throw out_of_range("Index is out of range!");
+  }
+
+  Node* newNode = new Node(e);
+
+  if (index == count) {  
+    newNode->prev = tail->prev;
+    newNode->next = tail;
+    tail->prev->next = newNode;
+    tail->prev = newNode;
+  } else {
+    Node* current = getPreviousNodeOf(index);
+    newNode->next = current->next;
+    newNode->prev = current;
+
+    if (current->next != nullptr) {
+      current->next->prev = newNode;
+    }
+    current->next = newNode;
+  }
+
+  count++;
+}
+
+template <class T>
+typename DLinkedList<T>::Node* DLinkedList<T>::getPreviousNodeOf(int index) {
+  if (index < 0 || index > count) {
+    throw out_of_range("Index is out of range!");
+  }
+
+  Node* current = head;
+  for (int i = 0; i < index; ++i) {
+    current = current->next;
+  }
+
+  return current;
+}
+
+template <class T>
+T DLinkedList<T>::removeAt(int index) {
+  if (index < 0 || index >= count) {
+    throw out_of_range("Index is out of range!");
+  }
+
+  Node* current = getPreviousNodeOf(index + 1);
+  T data = current->data;
+
+  if (current->prev != nullptr) {
+    current->prev->next = current->next;
+  }
+  if (current->next != nullptr) {
+    current->next->prev = current->prev;
+  }
+
+  delete current;
+
+  count--;
+
+  return data;
+}
+
+template <class T>
+bool DLinkedList<T>::empty() {
+  // TODO
+  return count == 0;
+}
+
+template <class T>
+int DLinkedList<T>::size() {
+  // TODO
+  return count;
+}
+
+template <class T>
+void DLinkedList<T>::clear() {
+  if (head == nullptr || tail == nullptr) {
+    return;  
+  }
+
+  Node* current = head->next;
+
+  while (current != tail) {
+    Node* nextNode = current->next;
+    delete current;
+    current = nextNode;
+  }
+
+  head->next = tail;
+  tail->prev = head;
+  count = 0;
+}
+
+template <class T>
+T& DLinkedList<T>::get(int index) {
+  if (index < 0 || index >= count) {
+    throw out_of_range("Index is out of range!");
+  }
+
+  Node* current = getPreviousNodeOf(index + 1);
+  return current->data;
+}
+
+template <typename T>
+int DLinkedList<T>::indexOf(T value) {
+  
+  if (head == nullptr) {
+    return -1;  
+  }
+
+  Node* current = head->next;  
+  int index = 0;               
+
+
+  while (current != tail) {
+    if (equals(current->data, value, this->itemEqual)) {
+      return index;  
+    }
+    current = current->next; 
+    index++;                  
+  }
+
+  return -1; 
+}
+
+template <class T>
+bool DLinkedList<T>::removeItem(T item, void (*removeItemData)(T)) {
+  Iterator it = begin();
+
+  while (it != end()) {
+    if (equals(*it, item, this->itemEqual)) {
+      it.remove(removeItemData);
+      return true;
+    }
+    ++it;
+  }
+
+  return false;
+}
+
+template <class T>
+bool DLinkedList<T>::contains(T item) {
+  Iterator it = begin();
+
+  while (it != end()) {
+    if (equals(*it, item, this->itemEqual)) {
+      return true;
+    }
+    ++it;
+  }
+
+  return false;
+}
+
+template <class T>
+string DLinkedList<T>::toString(string (*item2str)(T&)) {
+  if (head == nullptr || tail == nullptr) {
+    return "[]";
+  }
+
+  stringstream ss;
+  Node* current = head->next;
+
+  ss << "[";
+  while (current != tail) {
+    if (item2str != nullptr) {
+      ss << item2str(current->data);
+    } else {
+      ss << current->data;
+    }
+
+    current = current->next;
+    if (current != tail) {
+      ss << ", ";
+    }
+  }
+  ss << "]";
+
+  return ss.str();
+}
+
+template <class T>
+void DLinkedList<T>::copyFrom(const DLinkedList<T>& list) {
+  clear();
+
+  if (list.empty()) {
+    return;
+  }
+
+  for (Iterator it = list.begin(); it != list.end(); ++it) {
+    add(*it);
+  }
+}
+
+template <class T>
+void DLinkedList<T>::removeInternalData() {
+  Node* current = head->next;
+  while (current != tail) {
+    Node* nextNode = current->next;
+    if (deleteUserData) {
+      deleteUserData(current);
+    }
+    delete current;
+    current = nextNode;
+  }
+  head->next = tail;
+  tail->prev = head;
+  count = 0;
+}
+
+#endif /* DLINKEDLIST_H */
---
+++ include/loader/dataloader.h
@@ -0,0 +1,185 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt
+ * to change this license Click
+ * nbfs://nbhost/SystemFileSystem/Templates/cppFiles/file.h to edit this
+ * template
+ */
+
+/*
+ * File:   dataloader.h
+ * Author: ltsach
+ *
+ * Created on September 2, 2024, 4:01 PM
+ */
+
+#ifndef DATALOADER_H
+#define DATALOADER_H
+#include "ann/dataset.h"
+#include "ann/xtensor_lib.h"
+#include "list/listheader.h"
+
+using namespace std;
+// template<typename DType, typename LType>
+
+template <typename DType, typename LType>
+class DataLoader {
+ public:
+  class Iterator {
+   public:
+    Iterator(DataLoader &data_loader, int batch_index)
+        : data_loader(data_loader), batch_index(batch_index) {}
+
+    Batch<DType, LType> operator*() {
+      return data_loader.get_batch(batch_index);
+    }
+
+    Iterator &operator++() {
+      batch_index++;
+      return *this;
+    }
+    Iterator operator++(int) {
+      Iterator tmp = *this;
+      ++(*this);
+      return tmp;
+    }
+
+    bool operator!=(const Iterator &other) const {
+      return batch_index != other.batch_index;
+    }
+
+   private:
+    DataLoader &data_loader; 
+    int batch_index;
+  };
+
+ private:
+  Dataset<DType, LType> *ptr_dataset;
+  int batch_size;
+  bool shuffle;
+  bool drop_last;
+  int dataset_size;
+  int m_seed;
+  xt::xarray<unsigned long> indices; 
+  int total_batches;                  
+
+ public:
+  DataLoader(Dataset<DType, LType> *ptr_dataset, int batch_size,
+             bool shuffle = true, bool drop_last = false, int seed = -1) {
+    /*TODO: Add your code to do the initialization */
+    this->ptr_dataset = ptr_dataset;
+    this->batch_size = batch_size;
+    this->shuffle = shuffle;
+    this->drop_last = drop_last;
+    this->m_seed = seed;
+    dataset_size = ptr_dataset->len();
+    if (batch_size > dataset_size) {
+      total_batches = 0;
+    } else {
+      total_batches = dataset_size / batch_size;
+    }
+
+    // Resize and initialize indices vector
+    indices.resize({static_cast<size_t>(dataset_size)});
+    for (int i = 0; i < dataset_size; i++) {
+      indices[i] = i;
+    }
+
+    // Shuffle
+
+    if (shuffle) {
+      
+      xt::random::default_engine_type engine;  
+      if (m_seed >= 0) {
+        xt::random::seed(m_seed);  
+        engine.seed(m_seed);      
+      }
+      xt::random::shuffle(indices, engine); 
+    }
+  }
+
+  // Get Batch
+
+  Batch<DType, LType> get_batch(int batch_index) {
+    int start_index = batch_index * batch_size;
+    int end_index = std::min((batch_index + 1) * batch_size, dataset_size);
+
+    if (!drop_last && batch_index == total_batches - 1) {
+      end_index = dataset_size;  
+    }
+
+    int current_batch_size = end_index - start_index;
+
+
+    if (current_batch_size <= 0) {
+      throw std::runtime_error("Invalid batch size");
+    }
+    // Get shapes from the dataset
+    auto batch_data_shape = this->ptr_dataset->get_data_shape();
+    batch_data_shape[0] = current_batch_size;  // Update batch size in data shape
+    auto batch_label_shape = this->ptr_dataset->get_label_shape();
+    batch_label_shape[0] = current_batch_size;  // Update batch size in label shape
+
+    // Create xarrays for batch data and labels
+    xt::xarray<DType> batch_data = xt::empty<DType>(batch_data_shape);
+    xt::xarray<LType> batch_label = xt::empty<LType>(batch_label_shape);
+
+    // Populate the batch data and labels
+    for (int i = start_index; i < end_index; ++i) {
+      // try {
+      int index = indices[i]; 
+
+      DataLabel<DType, LType> data_label =
+          ptr_dataset->getitem(index);  
+
+      auto data = data_label.getData();   
+      auto label = data_label.getLabel(); 
+
+      // Ensure we are using the correct index to populate the batch arrays
+      xt::view(batch_data, i - start_index) = data;  
+      try {
+        if (this->ptr_dataset->get_label_shape().size() > 0) {
+          xt::view(batch_label, i - start_index) = label;  
+        } else {
+          batch_label(0) = (LType)0;
+        }
+      } catch (const std::bad_array_new_length &e) {
+      }
+      // } catch (const std::exception & e) {
+      //   std::cerr << "Error retrieving data for index " << indices[i] << ": "
+      //   << e.what() << std::endl;
+      //   // Handle the error appropriately; you might want to return a partial
+      //   batch return Batch < DType, LType > (batch_data, batch_label);
+      // }
+    }
+
+    return Batch<DType, LType>(batch_data, batch_label);  
+  }
+
+  Iterator begin() { return Iterator(*this, 0); }
+
+  Iterator end() {
+    // int dataset_len = ptr_dataset->len();
+    // int total_batches =
+    //     (dataset_len > batch_size) ? dataset_len / batch_size : 1;
+    // if (dataset_len < batch_size && total_batches == 1 && drop_last) {
+    //   total_batches = 0;
+    // }
+    return Iterator(*this, total_batches);
+  }
+
+  virtual ~DataLoader() {}
+
+  // /////////////////////////////////////////////////////////////////////////
+  // // The section for supporting the iteration and for-each to DataLoader //
+  // /// START: Section                                                     //
+  // /////////////////////////////////////////////////////////////////////////
+
+  // /*TODO: Add your code here to support iteration on batch*/
+
+  // /////////////////////////////////////////////////////////////////////////
+  // // The section for supporting the iteration and for-each to DataLoader //
+  // /// END: Section                                                       //
+  // /////////////////////////////////////////////////////////////////////////
+};
+
+#endif /* DATALOADER_H */
